c518caba37975473c74e98e98e458de9e960049a
Michael Lihs <mimi@kaktusteam.de>
2017-11-13 21:57:43 +0100

    add user get, ls, create command

--- cmd/user.go
+++ cmd/user.go
@@ -22,16 +22,66 @@ package cmd
 
 import (
 	"fmt"
+	"errors"
 
 	"github.com/spf13/cobra"
 	"github.com/xanzy/go-gitlab"
+	"github.com/spf13/viper"
+	"strconv"
 )
+
+var user, email, password, skype, linkedin, twitter, website_url, organization, username, extern_uid, provider, bio, location string
+var projects_limit int
+var admin, can_create_group, confirm, external bool
 
 // userCmd represents the user command
 var userCmd = &cobra.Command{
 	Use:   "user",
 	Short: "Manage Gitlab users",
 	Long: `Allows create, update and deletion of a user`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Use one of the subcommands, see `golab user -h`")
+	},
+}
+
+var getCmd = &cobra.Command{
+	Use: "get",
+	Short: "Get user details",
+	Long: `Get detailed information for given user`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if (id == "" && user == "") || (id != "" && user != "") {
+			return errors.New("you either have to provide an id or a username")
+		}
+		var userId int
+		if user != "" {
+			users, _, err := gitlabClient.Users.ListUsers(&gitlab.ListUsersOptions{Username: &user})
+			if err != nil {
+				return err
+			}
+			if len(users) != 1 {
+				return errors.New("Number of users found for username: " + strconv.Itoa(len(users)))
+			}
+			userId = users[0].ID
+		} else {
+			var err error = nil
+			userId, err = strconv.Atoi(id)
+			if err != nil {
+				return err
+			}
+		}
+		user, _, err := gitlabClient.Users.GetUser(userId)
+		if err != nil {
+			return err
+		}
+		err = OutputJson(user)
+		return err
+	},
+}
+
+var lsCmd = &cobra.Command{
+	Use: "ls",
+	Short: "Get list of all users",
+	Long: `Get a list of all users on the Gitlab server`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		users, _, err := gitlabClient.Users.ListUsers(&gitlab.ListUsersOptions{})
 		if err != nil {
@@ -46,9 +96,32 @@ var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create a new user",
 	Long: `Allows creation of a new user`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("create called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		createUserOptions := &gitlab.CreateUserOptions{
+			Admin: &admin,
+			Bio: &bio,
+			CanCreateGroup: &can_create_group,
+			Confirm: &confirm,
+			Email: &email,
+			Linkedin: &linkedin,
+			Name: &name,
+			Password: &password,
+			ProjectsLimit: &projects_limit,
+			Skype: &skype,
+			Twitter: &twitter,
+			Username: &username,
+			WebsiteURL: &website_url,
+		}
+		if provider != "" {
+			createUserOptions.Provider = &provider
+			createUserOptions.ExternUID = &extern_uid
+		}
+		user, _, err := gitlabClient.Users.CreateUser(createUserOptions)
+		if err != nil {
+			return err
+		}
+		err = OutputJson(user)
+		return err
 	},
 }
 
@@ -72,9 +145,60 @@ var updateCmd = &cobra.Command{
 	},
 }
 
+
 func init() {
+	initUserGetCommand()
+	userCmd.AddCommand(lsCmd)
+	initUserCreateCommand()
 	userCmd.AddCommand(updateCmd)
-	userCmd.AddCommand(createCmd)
 	userCmd.AddCommand(deleteCmd)
 	RootCmd.AddCommand(userCmd)
+}
+
+func initUserGetCommand() {
+	getCmd.PersistentFlags().StringVarP(&user, "user", "u", "", "(mandatory if id is unset) name of the user to look up")
+	getCmd.PersistentFlags().StringVarP(&id, "id", "i", "", "(mandatory if user is unset) id of the user to look up")
+	viper.BindPFlag("user", getCmd.PersistentFlags().Lookup("user"))
+	viper.BindPFlag("id", getCmd.PersistentFlags().Lookup("id"))
+	userCmd.AddCommand(getCmd)
+}
+
+func initUserCreateCommand() {
+	createCmd.PersistentFlags().StringVarP(&email, "email", "e", "", "(mandatory) Email")
+	createCmd.PersistentFlags().StringVarP(&password, "password", "p", "", "(mandatory) Password")
+	createCmd.PersistentFlags().StringVarP(&username, "username", "u", "", "(mandatory) Username")
+	createCmd.PersistentFlags().StringVarP(&name, "name", "n", "", "(mandatory) Name")
+	createCmd.PersistentFlags().StringVarP(&skype, "skype", "", "", "(optional) Skype ID")
+	createCmd.PersistentFlags().StringVarP(&linkedin, "linkedin", "", "", "(optional) LinkedIn")
+	createCmd.PersistentFlags().StringVarP(&twitter, "twitter", "", "", "(optional) Twitter account")
+	createCmd.PersistentFlags().StringVarP(&website_url, "website_url", "", "", "(optional) Website URL")
+	createCmd.PersistentFlags().StringVarP(&organization, "organization", "", "", "(optional) Organization name")
+	createCmd.PersistentFlags().IntVarP(&projects_limit, "projects_limit", "", 10, "(optional) Number of projects user can create (10 is default)")
+	createCmd.PersistentFlags().StringVarP(&extern_uid, "extern_uid", "", "", "(optional) External UID")
+	createCmd.PersistentFlags().StringVarP(&provider, "provider", "", "", "(optional) External provider name")
+	createCmd.PersistentFlags().StringVarP(&bio, "bio", "", "", "(optional) User's biography")
+	createCmd.PersistentFlags().StringVarP(&location, "location", "", "", "(optional) User's location")
+	createCmd.PersistentFlags().BoolVarP(&admin, "admin", "a", false, "(optional) User is admin - true or false (default)")
+	createCmd.PersistentFlags().BoolVarP(&can_create_group, "can_create_group", "", false, "(optional) User can create groups - true or false (default)")
+	createCmd.PersistentFlags().BoolVarP(&confirm, "confirm", "", true, "(optional) Require confirmation - true (default) or false")
+	createCmd.PersistentFlags().BoolVarP(&external, "external", "", false, "(optional) Flags the user as external - true or false(default)")
+	viper.BindPFlag("email", createCmd.PersistentFlags().Lookup("email"))
+	viper.BindPFlag("password", createCmd.PersistentFlags().Lookup("password"))
+	viper.BindPFlag("username", createCmd.PersistentFlags().Lookup("username"))
+	viper.BindPFlag("name", createCmd.PersistentFlags().Lookup("name"))
+	viper.BindPFlag("skype", createCmd.PersistentFlags().Lookup("skype"))
+	viper.BindPFlag("linkedin", createCmd.PersistentFlags().Lookup("linkedin"))
+	viper.BindPFlag("twitter", createCmd.PersistentFlags().Lookup("twitter"))
+	viper.BindPFlag("website_url", createCmd.PersistentFlags().Lookup("website_url"))
+	viper.BindPFlag("organization", createCmd.PersistentFlags().Lookup("organization"))
+	viper.BindPFlag("projects_limit", createCmd.PersistentFlags().Lookup("projects_limit"))
+	viper.BindPFlag("extern_uid", createCmd.PersistentFlags().Lookup("extern_uid"))
+	viper.BindPFlag("provider", createCmd.PersistentFlags().Lookup("provider"))
+	viper.BindPFlag("bio", createCmd.PersistentFlags().Lookup("bio"))
+	viper.BindPFlag("location", createCmd.PersistentFlags().Lookup("location"))
+	viper.BindPFlag("admin", createCmd.PersistentFlags().Lookup("admin"))
+	viper.BindPFlag("can_create_group", createCmd.PersistentFlags().Lookup("can_create_group"))
+	viper.BindPFlag("confirm", createCmd.PersistentFlags().Lookup("confirm"))
+	viper.BindPFlag("external", createCmd.PersistentFlags().Lookup("external"))
+	userCmd.AddCommand(createCmd)
 }
